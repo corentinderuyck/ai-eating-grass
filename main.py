8284ab17f56bff03037b108030e78cf9f1886f56
corentinderuyck <corentinderuyck1@gmail.com>
2024-12-03 17:05:08 +0100

    update and add wesite version

--- main.py
+++ main.py
@@ -1,244 +1,247 @@
 import pygame
 import random
 
+CELL_SIZE = 5
+WIDTH_SCREEN = 800
+HEIGHT_SCREEN = 600
 
-def main() :
+# Simulation parameters
+PROBA_COW_INIT = 10
+PROBA_GRASS_INIT = 40
+COW_ENERGY_INIT = 4
+COW_ENERGY_TO_MAKE_BABY = 5
+COW_LOSS_ENERGY_STEP = 1
+COW_INCREASE_ENERGY_EAT = 2
+PROBA_GRASS_GROW = 10
+TICK = -1
+
+def main():
     pygame.init()
 
-    # création de la map
-    map = generateMap()
+    # Create map
+    game_map = generate_map()
 
-    # création de la fenetre
-    fenetre = pygame.display.set_mode((800, 600))
+    # Create window
+    window = pygame.display.set_mode((WIDTH_SCREEN, HEIGHT_SCREEN))
 
-    # import cow/ grass
-    cow = pygame.image.load("src/cow.png")
-    grass = pygame.image.load("src/grass.png")
-
-    en_cours = True
-    while en_cours:
+    running = True
+    while running:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
-                en_cours = False
+                running = False
             elif event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_p:
-                    # pause
-                    pause = True
-                    while pause:
-                        for event_pause in pygame.event.get():
-                            if event_pause.type == pygame.KEYDOWN:
-                                if event_pause.key == pygame.K_p:
-                                    pause = False
                 if event.key == pygame.K_n:
-                    # new
-                    map = generateMap()
+                    # New map
+                    game_map = generate_map()
         
-        # affiche map
-        displayMap(map, fenetre, cow, grass)
+        # Display map
+        display_map(game_map, window)
 
-        # avance de 1 l'itération
-        nextIter(map)
+        # Advance to next iteration
+        next_iteration(game_map)
 
-        # vérifie si on a que de l'herbe
-        if(checkIfOnlyGrass(map)) :
-            # on regenere la map
-            map = generateMap()
+
+        # Check if only grass exists
+        if check_if_only_grass(game_map):
+            # Regenerate map
+            game_map = generate_map()
+
+        pygame.time.wait(TICK)
         
     pygame.quit()
 
-def checkIfOnlyGrass(map) :
+def check_if_only_grass(game_map):
     """
-    Vérifie si il n'y a que de l'herbe
+    Check if only grass exists on the map
     """
-
-    for y in range(len(map)) :
-        for x in range(len(map[0])) :
-            if map[y][x] != 1 :
+    for y in range(len(game_map)):
+        for x in range(len(game_map[0])):
+            if game_map[y][x] != 1:
                 return False
     return True
 
-def nextIter(map) :
+def next_iteration(game_map):
     """
-    Fait un pas dans l iteration
+    Perform one iteration step
     """
-    for y in range(len(map)) :
-        for x in range(len(map[0])) :
-            freePlace = checkFreePlace(map, y, x)
-            direction = random.randint(0, 3)
+    for y in range(len(game_map)):
+        for x in range(len(game_map[0])):
+            # Check if cow or grass
+            if not isinstance(game_map[y][x], Cow) and game_map[y][x] != 1:
+                continue
 
-            if(freePlace[direction]) :
-                # si libre
-                if(isinstance(map[y][x], Cow)) :
-                    # si vache
-                    currentCow = map[y][x]
+            # Check if cow
+            if isinstance(game_map[y][x], Cow):
+                # Make baby
+                if game_map[y][x].can_make_baby():
+                    direction_baby = random.randint(0, 3)
+                    is_free = check_free_place(game_map, y, x, direction_baby)
 
-                     # si peut faire un enfant on fait un enfant
-                    if(currentCow.canMakeBaby()) :
-                        freePlaceBaby = checkFreePlace(map, y, x)
-                        directionBaby = random.randint(0, 3)
-                        if(freePlaceBaby[directionBaby]) : 
-                            chanegMap(True, map, y, x, directionBaby, Cow())
+                    if is_free == 0 or is_free == 1:
+                        if direction_baby == 0:
+                            # Up
+                            game_map[y-1][x] = Cow()
+                        if direction_baby == 1:
+                            # Right
+                            game_map[y][x+1] = Cow()
+                        if direction_baby == 2:
+                            # Down
+                            game_map[y+1][x] = Cow()
+                        if direction_baby == 3:
+                            # Left
+                            game_map[y][x-1] = Cow()
 
-                    currentCow.reduceEnergie()
+                # Move cow
+                game_map[y][x].reduce_energy()
+                if game_map[y][x].no_energy():
+                    game_map[y][x] = 0
+                    continue
 
-                    if(currentCow.noEnergie()) :
-                        # si plus d energie
-                        map[y][x] = 0
-                    else :
-                        chanegMap(True, map, y, x, direction, currentCow)
+                direction = random.randint(0, 3)
+                item = check_free_place(game_map, y, x, direction)
 
-                if map[y][x] == 1 :
-                    # si herbe proba 1/4 de faire de l'herbe
-                    change = random.randint(0, 3)
-                    if (change == 0) :
-                        chanegMap(False, map, y, x, direction, None)
+                # move if not blocked or a cow
+                if item != -1 and item != 2:
+                    if item == 1:
+                        game_map[y][x].increase_energy()
+                    if direction == 0:
+                        game_map[y-1][x] = game_map[y][x]
+                    if direction == 1:
+                        game_map[y][x+1] = game_map[y][x]
+                    if direction == 2:
+                        game_map[y+1][x] = game_map[y][x]
+                    if direction == 3:
+                        game_map[y][x-1] = game_map[y][x]
+                    game_map[y][x] = 0
+                
+                            
+            # Check if grass
+            if game_map[y][x] == 1:
+                if random.randint(1, 100) <= PROBA_GRASS_GROW: 
+                    direction = random.randint(0, 3)
+                    item = check_free_place(game_map, y, x, direction)
 
-def chanegMap(cow, map, y, x, direction, currentCow) :
+                    if item == 0:
+                        if direction == 0:
+                            game_map[y-1][x] = 1
+                        if direction == 1:
+                            game_map[y][x+1] = 1
+                        if direction == 2:
+                            game_map[y+1][x] = 1
+                        if direction == 3:
+                            game_map[y][x-1] = 1
+
+
+def check_free_place(game_map, y, x, direction):
     """
-    Change la map pour la direction et l'item
+    Check if place is free
     """
-    if(cow == False) :
-        if(direction == 0) :
-            # haut
-            map[y-1][x] = 1
-        
-        if(direction == 1) :
-            # droite
-            map[y][x+1] = 1
 
-        if(direction == 2) :
-            # bas
-            map[y+1][x] = 1
-        
-        if(direction == 3) :
-            # gauche
-            map[y][x-1] = 1
+    # Up
+    if direction == 0:
+        if y == 0:
+            return -1
+        if game_map[y-1][x] == 0:
+            return 0
+        if game_map[y-1][x] == 1:
+            return 1
+        if isinstance(game_map[y-1][x], Cow):
+            return 2
     
-    if(cow == True) :
-        if(direction == 0) :
-            # haut
-            if(map[y-1][x] == 1) :
-                currentCow.increaseEnergie()
-            map[y-1][x] = currentCow
-        
-        if(direction == 1) :
-            # droite
-            if(map[y][x+1] == 1) :
-                currentCow.increaseEnergie()
-            map[y][x+1] = currentCow
+    # Right
+    if direction == 1:
+        if x == len(game_map[0]) - 1:
+            return -1
+        if game_map[y][x+1] == 0:
+            return 0
+        if game_map[y][x+1] == 1:
+            return 1
+        if isinstance(game_map[y][x+1], Cow):
+            return 2
 
-        if(direction == 2) :
-            # bas
-            if(map[y+1][x] == 1) :
-                currentCow.increaseEnergie()
-            map[y+1][x] = currentCow
-        
-        if(direction == 3) :
-            # gauche
-            if(map[y][x-1] == 1) :
-                currentCow.increaseEnergie()
-            map[y][x-1] = currentCow
+    # Down
+    if direction == 2:
+        if y == len(game_map) - 1:
+            return -1
+        if game_map[y+1][x] == 0:
+            return 0
+        if game_map[y+1][x] == 1:
+            return 1
+        if isinstance(game_map[y+1][x], Cow):
+            return 2
 
-        map[y][x] = 0
+    # Left
+    if direction == 3:
+        if x == 0:
+            return -1
+        if game_map[y][x-1] == 0:
+            return 0
+        if game_map[y][x-1] == 1:
+            return 1
+        if isinstance(game_map[y][x-1], Cow):
+            return 2
 
-def checkFreePlace(map, y, x) :
+def display_map(game_map, window):
     """
-    Renvoie une liste nous disant si on a une place de libre autour
-    la liste serra de ce style : [Haut, Droite, Bas, Gauche]
-    """
-    
-    checkedList = [False, False, False, False]
-
-    # haut
-    if(y != 0) :
-        if(map[y-1][x] == 0 or map[y-1][x] == 1) :
-            checkedList[0] = True
-    
-    # droite
-    if(x != len(map[0])-1) :
-        if(map[y][x+1] == 0 or map[y][x+1] == 1) :
-            checkedList[1] = True
-    
-    # bas
-    if(y != len(map)-1) :
-        if(map[y+1][x] == 0 or map[y+1][x] == 1) :
-            checkedList[2] = True
-    
-    # gauche
-    if(x != 0) :
-        if(map[y][x-1] == 0 or map[y][x-1] == 1) :
-            checkedList[3] = True
-
-    return checkedList
-
-def displayMap(map, fenetre, cow, grass) :
-    """
-    Affiche la map
+    Display the map
     """
 
-    # efface
-    fenetre.fill((0, 0, 0))
+    # Clear screen
+    window.fill((0, 0, 0))
 
-    for y in range(len(map)) :
-        for x in range(len(map[0])) :
-            if isinstance(map[y][x], Cow) :
-                fenetre.blit(cow, (x*5, y*5))
-            if map[y][x] == 1 :
-                fenetre.blit(grass, (x*5, y*5)) 
+    for y in range(len(game_map)):
+        for x in range(len(game_map[0])):
+            if isinstance(game_map[y][x], Cow):
+                pygame.draw.rect(window, (255, 255, 255), (x*CELL_SIZE, y*CELL_SIZE, CELL_SIZE, CELL_SIZE))
+            if game_map[y][x] == 1:
+                pygame.draw.rect(window, (0, 255, 0), (x*CELL_SIZE, y*CELL_SIZE, CELL_SIZE, CELL_SIZE))
 
     pygame.display.flip()
 
-def generateMap() :
+def generate_map():
     """
-    Genere la map
-    """
-
-    map = []
-    for i in range(120) :
-        subMap = []
-        for j in range(160) :
-            subMap.append(generateItem())
-        map.append(subMap)
-
-    return map
-
-def generateItem() :
-    """
-    Genere un item à mettre : une vache, de l'herbe ou rien
+    Generate map
     """
 
-    randNum = random.randint(1, 100)
-    
-    if (randNum == 1) :
-        # 1 % vaches
+    game_map = []
+    for i in range(HEIGHT_SCREEN // CELL_SIZE):
+        sub_map = []
+        for j in range(WIDTH_SCREEN // CELL_SIZE):
+            sub_map.append(generate_item())
+        game_map.append(sub_map)
+
+    return game_map
+
+def generate_item():
+    """
+    Generate an item: cow, grass, or nothing
+    """
+    if PROBA_COW_INIT + PROBA_GRASS_INIT > 100:
+        raise ValueError("The sum of the probabilities of cows and grass must be less than 100")
+
+    rand_num = random.randint(1, 100)
+    if 0 < rand_num <= PROBA_COW_INIT:
         return Cow()
     
-    if (randNum > 10 and randNum <= 40) :
-        # 30 % herbes
+    if PROBA_COW_INIT < rand_num <= PROBA_COW_INIT + PROBA_GRASS_INIT:
         return 1
     
     return 0
 
-
 class Cow:
     def __init__(self):
-        self.energie = 2
+        self.energy = COW_ENERGY_INIT
 
-    def canMakeBaby(self):
-        if(self.energie >= 2) :
-            return True
-        return False
+    def can_make_baby(self):
+        return self.energy >= COW_ENERGY_TO_MAKE_BABY
     
-    def reduceEnergie(self):
-        self.energie = self.energie - 1
+    def reduce_energy(self):
+        self.energy -= COW_LOSS_ENERGY_STEP
     
-    def noEnergie(self) : 
-        if(self.energie <= 0) :
-            return True
-        else :
-            return False
+    def no_energy(self):
+        return self.energy <= 0
 
-    def increaseEnergie(self):
-        self.energie = self.energie + 2
+    def increase_energy(self):
+        self.energy += COW_INCREASE_ENERGY_EAT
 
 main()
